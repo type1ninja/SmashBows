31545a1230ca11fecd5172c9660f3f21880ee9e0
Type1Ninja <type1ninja@gmail.com>
2017-01-29 13:56:05 -0600

    Navigable Main Menu
    There's now a bunch of buttons on the main menu for navigation. Next,
    I'll add functionality.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    private GameObject joinBtns;
+    private GameObject hostBtns;
+    private GameObject mainBtns;
+
+    private void Start()
+    {
+        joinBtns = transform.Find("JoinButtons").gameObject;
+        hostBtns = transform.Find("HostButtons").gameObject;
+        mainBtns = transform.Find("MainButtons").gameObject;
+
+        joinBtns.SetActive(false);
+        hostBtns.SetActive(false);
+        mainBtns.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            if (!mainBtns.activeSelf)
+            {
+                ReturnToMain();
+            }
+        }
+    }
+
+    //Start: Menu Navigation
+    public void OpenJoinBtns()
+    {
+        joinBtns.SetActive(true);
+
+        hostBtns.SetActive(false);
+        mainBtns.SetActive(false);
+    }
+
+    public void OpenHostBtns()
+    {
+        hostBtns.SetActive(true);
+
+        joinBtns.SetActive(false);
+        mainBtns.SetActive(false);
+    }
+
+    public void ReturnToMain()
+    {
+        mainBtns.SetActive(true);
+
+        joinBtns.SetActive(false);
+        hostBtns.SetActive(false);
+    }
+    //End: Menu Navigation
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
